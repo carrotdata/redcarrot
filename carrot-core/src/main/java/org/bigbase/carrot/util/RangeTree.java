67922482cdd56f7a4b3c38235166b96bd1b49b37
root <root@WinPro10.localdomain>
2021-11-27 00:20:27 -0800

    continue on mem test

--- carrot-core/src/main/java/org/bigbase/carrot/util/RangeTree.java
+++ carrot-core/src/main/java/org/bigbase/carrot/util/RangeTree.java
@@ -23,68 +23,79 @@ import org.apache.logging.log4j.Logger;
 
 public class RangeTree {
 
-    private static final Logger log = LogManager.getLogger(RangeTree.class);
+  private static final Logger log = LogManager.getLogger(RangeTree.class);
 
-    public static class Range implements Comparable<Range> {
-        long start;
-        int size;
+  public static class Range implements Comparable<Range> {
+    long start;
+    int size;
 
-        Range() {
-        }
+    Range() {}
 
-        Range(long start, int size) {
-            this.start = start;
-            this.size = size;
-        }
-
-        @Override
-        public int compareTo(Range o) {
-            return Long.compare(start, o.start);
-        }
+    Range(long start, int size) {
+      this.start = start;
+      this.size = size;
     }
 
-    private final TreeMap<Range, Range> treeMap = new TreeMap<>();
-
-    public RangeTree() {
+    @Override
+    public int compareTo(Range o) {
+      return Long.compare(start, o.start);
     }
 
-    public synchronized Range add(Range r) {
-        return treeMap.put(r, r);
+    @Override
+    public String toString() {
+      return "Range{" + "start=" + start + ", size=" + size + '}';
     }
+  }
 
-    public synchronized Range delete(long address) {
-        search.start = address;
-        search.size = 0;
-        return treeMap.remove(search);
+  private TreeMap<Range, Range> map = new TreeMap<>();
+
+  public RangeTree() {}
+
+  public synchronized Range add(Range r) {
+    return map.put(r, r);
+  }
+
+  public synchronized Range delete(long address) {
+    search.start = address;
+    search.size = 0;
+    return map.remove(search);
+  }
+
+  private final Range search = new Range();
+
+  public synchronized boolean inside(long start, int size) {
+    search.start = start;
+    search.size = size;
+    Range r = map.floorKey(search);
+    boolean result = !Objects.isNull(r) && start >= r.start && start + size <= r.start + r.size;
+
+    log.debug(
+        "floorKey start key {}, size key: {}, range: {}",
+        start,
+        size,
+        Objects.isNull(r) ? "null" : r.toString());
+    if (!result) {
+      if (!Objects.isNull(r)) {
+        log.debug(
+            "Check FAILED for range [{},{}] range allocation [{},{}] shift disposition [address: {}, size: {}]",
+            start,
+            size,
+            r.start,
+            r.size,
+            r.start - start,
+            +r.size - size);
+      } else {
+        log.debug("Check FAILED for range [{},{}] No allocation found.", start, size);
+      }
     }
+    return result;
+  }
 
-    private final Range search = new Range();
+  public synchronized int size() {
+    return map.size();
+  }
 
-    public synchronized boolean inside(long startKey, int sizeKey) {
-        search.start = startKey;
-        search.size = sizeKey;
-        Range rangeValue = treeMap.floorKey(search);
-//        boolean result = !Objects.isNull(rangeValue) && startKey >= rangeValue.start && startKey + sizeKey <= rangeValue.start + rangeValue.size;
-        boolean result = !Objects.isNull(rangeValue) && rangeValue.start + rangeValue.size >= rangeValue.start + sizeKey;
-
-        log.debug("floorKey start key {}, size key: {}, treeMap.size: {}", startKey, sizeKey, treeMap.size());
-        if (!result) {
-            if (!Objects.isNull(rangeValue)) {
-                log.debug(
-                        "Check FAILED for key [{},{}] range value allocation [{},{}] diff [{}, {}]",
-                        startKey, sizeKey, rangeValue.start, rangeValue.size, startKey - rangeValue.start, sizeKey - rangeValue.size);
-            } else {
-                log.debug("Check FAILED for key [{},{}] No value allocation found.", startKey, sizeKey);
-            }
-        }
-        return result;
-    }
-
-    public synchronized int size() {
-        return treeMap.size();
-    }
-
-    public synchronized Set<Map.Entry<Range, Range>> entrySet() {
-        return treeMap.entrySet();
-    }
+  public synchronized Set<Map.Entry<Range, Range>> entrySet() {
+    return map.entrySet();
+  }
 }
